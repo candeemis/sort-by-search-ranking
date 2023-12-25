747d85bb79e016d20196cb16d57ba1e35f675226
Nadeem Jamali <candeemis@users.noreply.github.com>
2023-12-25 17:20:46 +0100

    adds jaccard-index function together with tests in rust

--- rust_sort_by_search_score/src/lib.rs
+++ rust_sort_by_search_score/src/lib.rs
@@ -1,4 +1,5 @@
 use std::cmp::Ordering;
+use std::collections::HashSet;
 
 struct SourceScore {
     score: f32,
@@ -14,6 +15,31 @@ fn calculate_substring_score(search_term: &String, source: &String) -> f32 {
     } else {
         -1f32
     }
+}
+
+#[inline]
+fn calculate_jaccard_index(search_term: &String, source: &String) -> f32 {
+    if search_term.is_empty() || source.is_empty() {
+        return 0f32;
+    }
+
+    let mut search_term_chars: HashSet<char> = HashSet::new();
+    search_term.chars().for_each(|c| {
+        search_term_chars.insert(c);
+    });
+
+    let mut intersection: HashSet<char> = HashSet::new();
+
+    let mut source_chars: HashSet<char> = HashSet::new();
+    source.chars().for_each(|c| {
+        source_chars.insert(c);
+        if search_term_chars.contains(&c) {
+            intersection.insert(c);
+        }
+    });
+
+    intersection.len() as f32
+        / (search_term_chars.len() as f32 + source_chars.len() as f32 - intersection.len() as f32)
 }
 
 #[inline]
@@ -119,5 +145,43 @@ mod tests {
         ];
 
         assert_eq!(sorted, expected)
+    }
+
+    #[test]
+    fn it_calculates_jaccard_index_of_given_strings() {
+        let test_cases: Vec<(String, String, f32)> = vec![
+            (
+                String::from("berlin"),
+                String::from("berlino"),
+                0.85714287f32,
+            ),
+            (String::from("berlin"), String::from("berlin"), 1f32),
+            (String::from("berlin"), String::from("berl"), 0.6666667f32),
+            (String::from("berlin"), String::from("ber"), 0.5f32),
+            (String::from("berlin"), String::from("be"), 0.33333333f32),
+            (String::from("berlin"), String::from("b"), 0.16666667f32),
+            (
+                String::from("berlin"),
+                String::from("berlins"),
+                0.85714287f32,
+            ),
+            (String::from("hello"), String::from("hello"), 1f32),
+            (String::from("hello"), String::from("helo"), 1f32),
+            (String::from("hello"), String::from("world"), 0.2857143f32),
+            (String::from("pumpkin"), String::from("world"), 0f32),
+            (
+                String::from("pumpkin"),
+                String::from("pumpin"),
+                0.8333333f32,
+            ),
+            (String::from("pumpkin"), String::from("pumpkin"), 1f32),
+        ];
+
+        test_cases
+            .iter()
+            .for_each(|(search_term, source, expected)| {
+                let score = calculate_jaccard_index(search_term, source);
+                assert_eq!(score, *expected);
+            });
     }
 }
